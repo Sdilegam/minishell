30f289009d925b8c41d854061a9e6fb6190902e9
kuriiliin <kasmiabdoullah09@icloud.com>
2022-07-11 17:11:47 +0200

    outp redir not working well

--- srcs/outp_redir.c
+++ srcs/outp_redir.c
@@ -6,7 +6,7 @@
 /*   By: abkasmi <abkasmi@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:48:08 by abkasmi           #+#    #+#             */
-/*   Updated: 2022/07/11 17:08:46 by abkasmi          ###   ########.fr       */
+/*   Updated: 2022/07/11 17:10:54 by abkasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	outp_redir(t_comm *comm, t_env *env)
 	pid = fork();
 	if (pid == -1)
 		return (1);
-	file = open(comm->next->parameters[0], O_RDWR | O_CREAT, 0777);
 	if (pid == 0)
 	{
+		file = open(comm->next->parameters[0], O_RDWR | O_CREAT, 0777);
 		dup2(file, STDOUT_FILENO);
 		close(file);
 		function(comm, env);
