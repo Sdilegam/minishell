8f357fa982477101fa253e65aeb3b1d0f52ad2e2
kuriiliin <kasmiabdoullah09@icloud.com>
2022-07-11 17:07:26 +0200

    outp redirect not working well

--- srcs/outp_redir.c
+++ srcs/outp_redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   outp_redir.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
+/*   By: abkasmi <abkasmi@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:48:08 by abkasmi           #+#    #+#             */
-/*   Updated: 2022/07/11 14:33:07 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2022/07/11 17:04:31 by abkasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,19 @@
 
 int	outp_redir(t_comm *comm, t_env *env)
 {
-	(void)comm;
-	(void)env;
-	ft_printf("output redirection\n");
-	// int		fd[2];
-	// pid_t	id[2];
-
-	// id[0] = fork();
-	// if (id[0] == -1)
-	// 	return (1);
-	// if (id[0] == 0)
-	// {
-	// 	close(fd[0]);
-	// 	dup2(fd[1], STDIN_FILENO);
-	// 	close(fd[1]);
-	// 	function(comm, env);
-	// }
-	// id[1] = fork();
-	// if (id[1] == -1)
-	// 	return (1);
-	// if (id[1] == 0)
-	// {
-	// 	close(fd[1]);
-	// 	// if (>>)
-	// 	// 	dup2(fd[0], open(comm->parameters[1], O_CREAT | O_RDWR | O_APPEND));
-	// 	// else
-	// 		dup2(fd[0], open(comm->parameters[1], O_CREAT | O_RDWR));
-	// 	//ft_putstr_fd(fd[0], comm->parameters[])
-	// 	close(fd[0]);
-	// }
+	int		file;
+	pid_t	pid;
+	
+	pid = fork();
+	if (pid == -1)
+		return (1);
+	file = open(comm->next->parameters[0], O_RDWR | O_CREAT, 0777);
+	if (pid == 0)
+	{
+		dup2(file, STDOUT_FILENO);
+		close(file);
+		function(comm, env);
+	}
+	close(file);
 	return (0);
 }
