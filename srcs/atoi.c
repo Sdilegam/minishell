3c87ae751cbef7712b86ce2934fdd6fc94d2c24b
sdi-lega <sdi-lega@student.s19.be>
2022-07-15 14:50:09 +0200

    Atoi complet

--- srcs/atoi.c
+++ srcs/atoi.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   atoi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abkasmi <abkasmi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:04:38 by abkasmi           #+#    #+#             */
-/*   Updated: 2022/07/13 15:05:31 by abkasmi          ###   ########.fr       */
+/*   Updated: 2022/07/15 14:49:37 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <limits.h>
+
+static int	ft_overflow(const char *str, int signe, int i)
+{
+	unsigned long long	num;
+
+	num = 0;
+	while (str[i] > 47 && str[i] < 58)
+	{
+		num = (num * 10) + (str[i] - 48);
+		if (num >= LONG_MAX && signe == 1)
+			return (-1);
+		else if (num > LONG_MAX && signe == -1)
+			return (0);
+		i++;
+	}
+	return (num);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -29,6 +47,6 @@ int	ft_atoi(const char *str)
 			signe = -signe;
 		i++;
 	}
-	num = (num * 10) + (str[i] - 48);
+	num = ft_overflow(str, signe, i);
 	return (num * signe);
 }
